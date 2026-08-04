f9e52edef3d8b217f949eaa0795336762a0e44bb
rood107 <rodrigoalave107@gmail.com>
2026-08-04 14:50:37 -0400

    Se creo el componente hijo para registro de tareas

---
+++ tareas_frontend/src/app/components/tarea/reg-tarea/reg-tarea.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reg-tarea',
+  imports: [],
+  templateUrl: './reg-tarea.html',
+  styleUrl: './reg-tarea.css',
+})
+export class RegTarea {
+
+}
